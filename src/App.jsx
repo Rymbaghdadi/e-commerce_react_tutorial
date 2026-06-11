979d3230cdd5ec7829fdb5e3ae298ba2fb3f5f47
Rym <rimbaghdadi22@gmail.com>
2026-06-11 16:36:12 +0300

    edited home page

--- src/App.jsx
+++ src/App.jsx
@@ -1,68 +1,21 @@
 import './App.css'
 import { useState } from 'react';
 import { Route , Routes } from 'react-router-dom'
+import Home from './pages/Home';
+import Auth from './pages/Auth';
+import Checkout from './pages/Checkout';
+import Navbar from './components/Navbar';
 function App() {
-
- function HomePage() {
-    return <h1>Home Page</h1>
-  }
-
-  function AboutPage() {
-    return <h1>About Page</h1>
-  }
-
-
-function LoginPage() {
-  const [inputName, setInputName] = useState("");
-  const [user, setUser] = useState({
-    name: "",
-    isAuth: false,
-  });
-
-  const handleSubmit = (e) => {
-    e.preventDefault();
-
-    setUser({
-      name: inputName,
-      isAuth: true,
-    });
-  };
-
-  if (user.isAuth) {
-    return (
-      <div>
-        <h2>Welcome {user.name} 🎉</h2>
-      </div>
-    );
-  }
-
-  return (
-    <div>
-      <h2>Login</h2>
-
-      <form onSubmit={handleSubmit}>
-        <input
-          type="text"
-          placeholder="Enter your name"
-          value={inputName}
-          onChange={(e) => setInputName(e.target.value)}
-        />
-
-        <button type="submit">Login</button>
-      </form>
-    </div>
-  );
-}
-  return (
-    <>
+ return (
+  <div className="App">
+    <Navbar />
     <Routes>
-      <Route path="/" element={<HomePage />} />
-      <Route path="/login" element={<LoginPage />} />
-      <Route path="/about" element={<AboutPage />} />
-      <Route path="*" element={<h1>404 not found</h1>} />
-    </Routes>   
-    </>
-  )
+      <Route path='/' element={<Home />} />
+      <Route path='/auth' element={<Auth />} />
+      <Route path='/checkout' element={<Checkout />} />
+    </Routes>
+  </div>
+ )
 }
 
 export default App
